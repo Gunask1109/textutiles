300ce9882545f68a3db9caaf147404481e52666c
Gunask1109 <guna.sekar7045@gmail.com>
2020-06-16 13:18:26 +0530

    Second commit

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -8,10 +8,13 @@ def index(request):
 #pipline
 def analyze(request):
     #get the text
-    djtext=request.GET.get('text','default')
-    removepunc = request.GET.get('removepunc', 'off')
-    print(removepunc)
-    print(djtext)
+    djtext=request.POST.get('text','default')
+    #check checkbox value
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover= request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
     #analyzed= djtext
     if removepunc == "on":
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*'''
@@ -20,9 +23,51 @@ def analyze(request):
             if char not in punctuations:
                 analyzed+=char
         params ={'purpose':'Removed Punctuations','analyzed_text':analyzed}
-        return render(request,'analyze.html',params)
-    else:
-        return HttpResponse("Error")
+        djtext=analyzed
+        #return render(request,'analyze.html',params)
+
+    if fullcaps == "on":
+        analyzed=""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if extraspaceremover == "on":
+        analyzed = ""
+        for index,char in enumerate(djtext):
+            if djtext[index] == " " and djtext[index+1]== "":
+                pass
+            else:
+                analyzed = analyzed + char
+        params = {'purpose': 'Extra Space Remover', 'analyzed_text': analyzed}
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if newlineremover == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed New Line', 'analyzed_text': analyzed}
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+    if charcount == "on":
+        analyzed = 0
+        for char in djtext:
+            analyzed+=1
+        params = {'purpose': 'Count Of Character ', 'analyzed_text': analyzed}
+        djtext = analyzed
+        #return render(request, 'analyze.html', params)
+
+
+
+    #else:
+        #return HttpResponse("Error")
+    return render(request, 'analyze.html', params)
+
+
+
 
 #def capfirst(request):
  #   return HttpResponse("capitalize first")
